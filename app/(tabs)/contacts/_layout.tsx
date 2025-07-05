8871a1cde706a86a94f45b726bf809e46b5b6b56
Adam Pagels <pagels@hotmail.ca>
2025-07-05 22:54:12 +0100

    Update back button style

--- app/(tabs)/contacts/_layout.tsx
+++ app/(tabs)/contacts/_layout.tsx
@@ -4,7 +4,16 @@ import { Stack } from "expo-router";
 export default function ContactsLayout() {
   return (
     <ContactsProvider>
-      <Stack />
+      <Stack>
+        <Stack.Screen name="index" options={{ title: "Contacts" }} />
+        <Stack.Screen
+          name="[id]"
+          options={{
+            title: "",
+            headerBackButtonDisplayMode: "minimal",
+          }}
+        />
+      </Stack>
     </ContactsProvider>
   );
 }
